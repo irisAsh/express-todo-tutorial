68cd50d5fc36f12cf07b4f56025a26e27c6825d7
irisAsh <043takotoka@gmail.com>
2019-03-02 23:35:35 +0900

    Fetch todos

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,5 +1,49 @@
-exports.index = function(req, res) {
-  res.render('todo/index');
+var MongoClient = require('mongodb').MongoClient;
+var dbName = 'express-todo-tutorial'
+var url = 'mongodb://127.0.0.1:27017/' + dbName;
+
+exports.index = function(req, res, next) {
+  var connectionToDB = function() {
+    return new Promise(function(resolve, reject) {
+      MongoClient.connect(url, {useNewUrlParser: true}, function(error, client) {
+        if (error) {
+          reject(error);
+        } else {
+          var db = client.db(dbName);
+          resolve({ client, db });
+        }
+      });
+    });
+  };
+
+  var getTodos = function({ client, db }) {
+    return new Promise(function(resolve, reject) {
+      db.collection('todos')
+      .find({})
+      .toArray(function(error, docs) {
+        if (error) {
+          reject(error);
+        } else {
+          client.close();
+          resolve(docs);
+        }
+      });
+    });
+  }
+
+  connectionToDB()
+  .then(function(db) {
+    return getTodos(db);
+  })
+  .then(function(result) {
+    res.render('todo/index', {
+      todos: result
+    });
+  })
+  .catch(function(err) {
+    console.log(err);
+    next(err);
+  });
 };
 
 exports.today = function(req, res) {
